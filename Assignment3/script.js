2f34061b7494857af8b4ee25c1271dcd8fbeef80
Saketh Vallakatla <saketh21v@gmail.com>
2017-08-24 14:40:47 +0530

    Added fields for each shape

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -1,3 +1,42 @@
+function loadForm(e) {
+    var inputForm = document.getElementById('inputForm');
+    inputForm.innerHTML = "";
+    var radios = document.getElementsByName("shape");
+    var option;
+    radios.forEach(function(element) {
+        if (element.checked)
+            option = element.value;
+    }, this);
+    switch (option) {
+        case "Rect":
+            var widthInput = document.createElement("input");
+            widthInput.placeholder = "Width";
+            widthInput.type = "Number";
+            var heightInput = document.createElement("input");
+            heightInput.placeholder = "Height";
+            heightInput.type = "Number";
+            inputForm.appendChild(widthInput);
+            inputForm.appendChild(heightInput);
+            break;
+        case "Circ":
+            var radiusInput = document.createElement("input");
+            radiusInput.type = "number";
+            radiusInput.placeholder = "Radius";
+            inputForm.appendChild(radiusInput);
+            break;
+        default:
+            break;
+    }
+}
+
+window.onload = function(e) {
+    // alert("load");
+    var radios = document.getElementsByName("shape");
+    radios.forEach(function(element) {
+        element.onclick = loadForm;
+    }, this);
+}
+
 function validate() {
     var canvas = document.getElementById("can");
     var radios = document.getElementsByName("shape");
