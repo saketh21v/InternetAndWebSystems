15d824f36ffe2f428c74f839f33e08106b933a71
Saketh Vallakatla <saketh21v@gmail.com>
2017-09-04 12:38:38 +0530

    added map

---
+++ Quora_Interface/js/script.js
@@ -0,0 +1,23 @@
+function openTrending(){
+    var main = document.getElementById("Main");
+    main.childNodes[1].setAttribute("hidden", true);
+    main.childNodes[3].removeAttribute("hidden");
+}
+
+function openHome(){
+    var main = document.getElementById("Main");
+    main.childNodes[3].setAttribute("hidden", true);
+    main.childNodes[1].removeAttribute("hidden");
+}
+
+function addTopic() {
+    var nav = document.getElementById("left-nav-bar");
+    var text = prompt("Enter the topic name");
+    var ul = nav.children[0];
+    var newLi = document.createElement('li');
+    var ahref = document.createElement('a');
+    ahref.setAttribute('href', '#');
+    ahref.innerHTML = text;
+    newLi.appendChild(ahref);
+    ul.appendChild(newLi);
+}
