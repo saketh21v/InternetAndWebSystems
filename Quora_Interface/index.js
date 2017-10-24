49738f503f42d6580a8aeb5a5a39c681e064ef23
Saketh Vallakatla <saketh21v@gmail.com>
2017-10-24 17:02:57 +0530

    Added Continent map

--- Quora_Interface/index.js
+++ Quora_Interface/index.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const fs = require('fs');
+const request = require('request');
 const express = require('express');
 const app = express();
+
+const _GEOLOC_API = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=';
+const _CONTINET = require("./lib/continents");
 
 let cat = "physics";
 
@@ -11,23 +15,44 @@ app.use(express.static('public'));
 app.get('/index', function (req, res) {
     cat = "physics";
     let topic = req.query.topic;
-    if(topic != undefined){
+    if (topic != undefined) {
         cat = topic;
-        console.log("cat: "+ topic);
+        console.log("cat: " + topic);
     }
     fs.readFile('public/index.html', (err, mData) => {
-        fs.readFile('categories/'+cat+".html", (err, data) => {
-            if(err){
-                let mDataStr = mData.toString();            
-                mDataStr = mDataStr.replace("###%%%###", "Sorry! No such topic");            
+        fs.readFile('categories/' + cat + ".html", (err, data) => {
+            if (err) {
+                let mDataStr = mData.toString();
+                mDataStr = mDataStr.replace("###%%%###", "Sorry! No such topic");
                 return res.end(mDataStr);
             }
             let mDataStr = mData.toString();
             let stuff = data.toString();
             mDataStr = mDataStr.replace("###%%%###", data);
-            res.end(mDataStr);            
+            res.end(mDataStr);
         });
     });
+});
+
+app.get('/location', (req, res) => {
+    let lat = req.query.lat;
+    let long = req.query.long;
+
+    if (lat == undefined || long == undefined) {
+        res.end("Error");
+    } else {
+        request(_GEOLOC_API + lat + ',' + long, (err, body, result) => {
+            if(err){
+                return console.log("Error!!!");
+            }
+            console.log("BODY: " + body.toString());
+            var r = JSON.parse(body);
+            var countryID = res.results[res.results.length - 1].address_components[0].short_name;
+            console.log("CONTINENT : " + _CONTINENT[countryID]);
+            res.end(_CONTINENT[countryID]);
+        });
+    }
+    res.end("ERROR");
 });
 
 app.listen(3000, function () {
--- Quora_Interface/public/js/script.js
+++ Quora_Interface/public/js/script.js
@@ -1,10 +1,11 @@
-function openTrending(){
+
+function openTrending() {
     var main = document.getElementById("Main");
     main.childNodes[1].setAttribute("hidden", true);
     main.childNodes[3].removeAttribute("hidden");
 }
 
-function openHome(){
+function openHome() {
     var main = document.getElementById("Main");
     main.childNodes[3].setAttribute("hidden", true);
     main.childNodes[1].removeAttribute("hidden");
@@ -22,7 +23,7 @@ function addTopic() {
     ul.appendChild(newLi);
 }
 
-function changeCat(topic){
+function changeCat(topic) {
     console.log(topic);
     document.getElementById('inputCat').value = topic;
     document.forms[0].submit();
